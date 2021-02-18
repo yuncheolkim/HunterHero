79b9972726f3afc10ba5d05f4fb792a38687bcd7
yunzz <yuncheolkim@163.com>
2021-02-18 17:17:28 +0800

    spring boot

---
+++ src/main/java/game/GameMain.java
@@ -0,0 +1,13 @@
+package game;
+
+/**
+ * @author Yunzhe.Jin
+ * 2021/2/18 16:13
+ */
+public class GameMain {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("start");
+        Thread.sleep(5000);
+
+    }
+}
