0cb24f8971c5360d08807e1bbf1a4b157cf65fea
yunzz <yuncheolkim@163.com>
2021-04-08 18:59:06 +0800

    formation

---
+++ src/main/java/game/module/formation/FormationHandler.java
@@ -0,0 +1,17 @@
+package game.module.formation;
+
+import game.player.Player;
+import game.proto.HeroEquipmentReq;
+
+/**
+ * 阵型
+ *
+ * @author Yunzhe.Jin
+ * 2021/4/8 17:35
+ */
+public class FormationHandler {
+
+    public static void create(Player player, HeroEquipmentReq req) {
+
+    }
+}
