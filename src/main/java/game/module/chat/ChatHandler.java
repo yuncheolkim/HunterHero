d5aeeb98eeb7a26b202be4c6332d2001f33cb128
yunzz <yuncheolkim@163.com>
2021-05-26 10:11:14 +0800

    1

--- src/main/java/game/module/chat/ChatHandler.java
+++ src/main/java/game/module/chat/ChatHandler.java
@@ -13,7 +13,6 @@ import game.proto.ChatMessageReq;
  */
 public class ChatHandler {
 
-
     /**
      * 聊天
      *
@@ -27,33 +26,4 @@ public class ChatHandler {
         ChatScene chatScene = G.G.getChatScene();
         chatScene.tell(new ChatMessage(player.getPid(), req));
     }
-
-
-    /**
-     * 世界聊天
-     *
-     * @param player
-     */
-    public static void worldChat(Player player) {
-
-    }
-
-    /**
-     * 帮派聊天
-     *
-     * @param player
-     */
-    public static void groupChat(Player player) {
-
-    }
-
-    /**
-     * 私聊
-     *
-     * @param player
-     */
-    public static void privacyChat(Player player) {
-
-    }
-
 }
