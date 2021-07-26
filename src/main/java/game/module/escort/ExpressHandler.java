4f8e2ef017594afa14e7871bec0ce5002bb68eec
yuncheolkim@163.com <yuncheolkim@163.com>
2021-07-26 23:25:37 +0800

    express

---
+++ src/main/java/game/module/escort/ExpressHandler.java
@@ -0,0 +1,24 @@
+package game.module.escort;
+
+import game.player.Player;
+import game.proto.FightStartReq;
+
+/**
+ * 跑镖
+ *
+ * @author Yunzhe.Jin
+ * 2021/7/26 22:26
+ */
+public class ExpressHandler {
+
+    /**
+     * 开始跑镖
+     *
+     * @param player
+     * @param req
+     */
+    public static void start(final Player player, final FightStartReq req) {
+
+    }
+
+}
